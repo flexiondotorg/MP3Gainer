2f90ba72f7b44ef769373e77bf9395066d7cabad
Martin Wimpress <code@flexion.org>
2009-09-20 19:21:37 +0100

    Updated release script

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 function build {
     RELEASE_NAME="MP3Gainer"
-    RELEASE_VER=1.0
+    RELEASE_VER="1.0"
+    RELEASE_DESC="Applies ReplayGain to a MP3 music collection"
+    RELEASE_KEYWORDS="replaygain, replay, gain, MP3, script, automatic, music, normalise, volume, album, track"
 
     rm ${RELEASE_NAME}-v${RELEASE_VER}.tar* 2>/dev/null
     bzr export ${RELEASE_NAME}-v${RELEASE_VER}.tar
